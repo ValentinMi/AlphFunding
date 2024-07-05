859f3c177db4845fde17cb17fc14028cfa922c2a
Valentin Misiaszek <valentin.misiaszek@gaia-x.eu>
2024-07-05 15:51:35 +0200

    feat(app): create pool form UI

--- app/components/PoolForm.tsx
+++ app/components/PoolForm.tsx
@@ -1,37 +1,151 @@
 import React from "react";
-import { Box, Button, Flex, FormControl, FormLabel, Input, Textarea, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormErrorMessage,
+  FormHelperText,
+  FormLabel,
+  Input,
+  NumberDecrementStepper,
+  NumberIncrementStepper,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  Textarea,
+  VStack
+} from "@chakra-ui/react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { isDateAtLeastOneWeekInFuture } from "../utils";
 
 interface PoolFormProps {}
 
+type Inputs = {
+  name: string;
+  beneficiary: string;
+  goal: number;
+  end: string;
+  description: string;
+};
+
 export const PoolForm: React.FC<PoolFormProps> = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
   return (
     <Flex w={"100%"} justifyContent={"center"}>
       <Box w={"60%"}>
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <VStack spacing={5}>
-            <FormControl>
+            <FormControl isInvalid={!!errors.name}>
               <FormLabel>Name</FormLabel>
-              <Input type="text" />
+              <Input
+                type="text"
+                {...register("name", {
+                  required: true,
+                  minLength: 5,
+                  maxLength: 50,
+                })}
+              />
+              {!!errors.name && (
+                <FormErrorMessage>
+                  {errors.name.type === "required"
+                    ? "This field is required"
+                    : errors.name.type === "minLength"
+                      ? "Name should be at least 5 characters"
+                      : "Name should be at most 50 characters"}
+                </FormErrorMessage>
+              )}
             </FormControl>
-            <FormControl>
+            <FormControl isInvalid={!!errors.beneficiary}>
               <FormLabel>Beneficiary address</FormLabel>
-              <Input type="text" />
+              <Input
+                type="text"
+                {...register("beneficiary", {
+                  required: true,
+                  pattern: /[1-9A-HJ-NP-Za-km-z]{40,}$/,
+                })}
+              />
+              {!!errors.beneficiary && (
+                <FormErrorMessage>
+                  {errors.beneficiary.type === "required"
+                    ? "This field is required"
+                    : "Invalid Alephium address"}
+                </FormErrorMessage>
+              )}
             </FormControl>
-            <FormControl>
+            <FormControl isInvalid={!!errors.goal}>
               <FormLabel>Goal</FormLabel>
-              <Input type="text" />
+              <NumberInput defaultValue={1} min={0.1}>
+                <NumberInputField
+                  {...register("goal", { required: true, min: 0.1 })}
+                />
+                <NumberInputStepper>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+              </NumberInput>
+              {!!errors.goal && (
+                <FormErrorMessage>
+                  {errors.goal.type === "required"
+                    ? "This field is required"
+                    : "Goal should be at least 0.1 ALPH"}
+                </FormErrorMessage>
+              )}
             </FormControl>
-            <FormControl>
-              <FormLabel>End</FormLabel>
-              <Input type="text" />
+            <FormControl isInvalid={!!errors.end}>
+              <FormLabel>End date</FormLabel>
+              <Input
+                type="date"
+                {...register("end", {
+                /*  required: true,
+                  pattern:
+                    /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[012])\/(19|20)\d\d$/,*/
+                  validate: (value) => isDateAtLeastOneWeekInFuture(value),
+                })}
+              />
+              {!!errors.end ? (
+                <FormErrorMessage>
+                  {errors.end.type === "required"
+                    ? "This field is required"
+                    : "Invalid date"}
+                </FormErrorMessage>
+              ) : (
+                <FormHelperText>
+                  The end date need to be at least one week in the future
+                </FormHelperText>
+              )}
             </FormControl>
-            <FormControl>
+            <FormControl isInvalid={!!errors.description}>
               <FormLabel>Description</FormLabel>
-              <Textarea />
+              <Textarea
+                {...register("description", {
+                  required: true,
+                  minLength: 100,
+                  maxLength: 1000,
+                })}
+              />
+              {!!errors.description && (
+                <FormErrorMessage>
+                  {errors.description.type === "required"
+                    ? "This field is required"
+                    : errors.description.type === "minLength"
+                      ? "Description should be at least 100 characters"
+                      : "Description should be at most 1000 characters"}
+                </FormErrorMessage>
+              )}
             </FormControl>
           </VStack>
           <Flex justifyContent={"center"}>
-            <Button mt={6}>Submit</Button>
+            <Button colorScheme={"green"} type={"submit"} mt={6}>
+              Submit
+            </Button>
           </Flex>
         </form>
       </Box>
--- app/utils.ts
+++ app/utils.ts
@@ -20,3 +20,24 @@ export const truncateText = (text: string, maxLength: number): string  => {
 
     return truncatedText + '...';
 }
+
+
+export const dateToTimestamp = (dateString: string): number => {
+    console.log("toto", dateString);
+    const [year, month, day] = dateString.split('-').map(Number);
+    const dateObject = new Date(year, month - 1, day);
+    return dateObject.getTime() / 1000;
+}
+
+export const isDateAtLeastOneWeekInFuture = (dateString: string): boolean => {
+    const oneWeekInSeconds = 60 * 60 * 24 * 7;
+
+    const inputDateInTimestamp = dateToTimestamp(dateString);
+
+    const todayTimestamp = new Date().getTime() / 1000;
+
+    console.log(inputDateInTimestamp, todayTimestamp, inputDateInTimestamp - todayTimestamp >= oneWeekInSeconds);
+
+    return inputDateInTimestamp - todayTimestamp >= oneWeekInSeconds;
+}
+
