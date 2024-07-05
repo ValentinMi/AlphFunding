055644bf161300a8c22323511f0cde45b140845c
Valentin Misiaszek <valentin.misiaszek@gaia-x.eu>
2024-07-05 02:25:09 +0200

    feat: add color mode btn

--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -1,13 +1,16 @@
 import React from "react";
-import { Box, Flex, Heading } from "@chakra-ui/react";
+import { Box, Flex, Heading, IconButton, useColorMode } from "@chakra-ui/react";
 import { AlephiumConnectButton } from "@alephium/web3-react";
 import { Account } from "@alephium/web3";
 import { sliceAddress } from "../../utils";
 import NextLink from "next/link";
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 
 interface HeaderProps {}
 
 export const Header: React.FC<HeaderProps> = () => {
+  const { colorMode, toggleColorMode } = useColorMode()
+
   const displayAccount = (account: Account) => {
     const address = account.address;
     return sliceAddress(address);
@@ -15,8 +18,13 @@ export const Header: React.FC<HeaderProps> = () => {
 
   return (
     <Flex w={"100%"} justifyContent={"space-between"} p={3}>
-      <Box as={NextLink} href={"/"}><Heading>AlphPool</Heading></Box>
-      <AlephiumConnectButton displayAccount={displayAccount} />
+      <Box as={NextLink} href={"/"}>
+        <Heading>AlphPool</Heading>
+      </Box>
+      <Flex>
+        <IconButton aria-label={"toto"} mr={3} icon={colorMode === "light" ? <MoonIcon/> : <SunIcon />} onClick={toggleColorMode} />
+        <AlephiumConnectButton displayAccount={displayAccount} />
+      </Flex>
     </Flex>
   );
 };
